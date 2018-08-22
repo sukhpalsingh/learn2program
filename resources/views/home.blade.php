21df50b6dd4715d8054aa79000634a5c9f512c69
Sukhpal Singh <sukhpalsingh@noreply.users.github.com>
2018-08-22 20:49:54 +1000

    Adding laravel 5.5 with mdbootstrap

---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>App</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <link href="/storage/css/lib.css" rel="stylesheet" type="text/css">
+
+        <style>
+            html, body {
+                font-family: 'Raleway', sans-serif;
+            }
+        </style>
+    </head>
+    <body>
+
+        <script src="/storage/js/lib.js" type="text/javascript"></script>
+    </body>
+</html>
---
+++ webpack.mix.js
@@ -0,0 +1,25 @@
+let mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.styles([
+    'node_modules/mdbootstrap/css/bootstrap.min.css',
+    'node_modules/mdbootstrap/css/mdb.min.css',
+    'resources/assets/css/app.css'
+], 'public/storage/css/lib.css');
+
+mix.scripts([
+    'node_modules/mdbootstrap/js/jquery-3.3.1.min.js',
+    'node_modules/mdbootstrap/js/popper.min.js',
+    'node_modules/mdbootstrap/js/bootstrap.min.js',
+    'resources/assets/js/app.js'
+], 'public/storage/js/lib.js');
